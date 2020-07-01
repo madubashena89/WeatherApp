a938e4e6eb9568fa8e9be5105e8480e5330c7098
madubashena89 <madubashena89@gmail.com>
2020-07-01 00:43:00 -0500

    search by city name added

--- app/src/main/java/top/stores/weatherapp/MainActivity.java
+++ app/src/main/java/top/stores/weatherapp/MainActivity.java
@@ -9,19 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -76,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-         weatherDataSeervice.getCityForecastByID(et_dataInput.getText().toString(), new WeatherDataSeervice.ForecastByIDResponse() {
+              weatherDataSeervice.getCityForecastByID(et_dataInput.getText().toString(), new WeatherDataSeervice.ForecastByIDResponse() {
              @Override
              public void onError(String message) {
                  Toast.makeText(MainActivity.this,"Something went wrong..!", Toast.LENGTH_SHORT).show();
